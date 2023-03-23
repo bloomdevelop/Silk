20e0c260e4e5242ac8a43ad9644cbf49e315c569
StationaryStation <raulytstation@outlook.com>
2023-03-23 08:58:49 -0400

    Add ?dog command

---
+++ src/commands/fun/dog.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Message } from "revolt.js";
+
+const dog: Command = {
+    name: "dog",
+    description:
+        "Sends a random image of a dog. Powered by random.dog",
+    args: false,
+    async execute(message: Message) {
+        axios.get("https://random.dog/woof?filter=mp4,webm").then(async (res) => {
+            await message.reply(`[dog :3](https://random.dog/${res.data})`);
+        });
+    },
+};
+
+export = dog;
