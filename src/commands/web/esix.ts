1f6bba9bbb8f06aa3acdc6e15c3e261464eb3bae
StationaryStation <raulytstation@outlook.com>
2023-03-24 09:59:08 -0400

    fix: use posts.lenght instead of hardcoded value

--- src/commands/web/esix.ts
+++ src/commands/web/esix.ts
@@ -11,10 +11,9 @@ const esix: Command = {
             const post = await esixAPI.posts.search({
                 limit: 100,
                 tags: `rating:safe ${args.join(" ")}`,
-            }).then(posts => posts[Math.floor(Math.random() *  (Math.floor(100) - Math.ceil(0) + 1)) + Math.ceil(0)]).catch(e => {
+            }).then(posts => posts[Math.floor(Math.random() *  (Math.floor(posts.length) - Math.ceil(0) + 1)) + Math.ceil(0)]).catch(e => {
                 throw e;
             })
-    
             return await message.reply(`[Result from E621](${post.file.url})`);
         } catch (error) {
             return await message.reply(`Error trying to find "rating:safe ${args.join(" ")}"\n\`\`\`\n${error}\n\`\`\``);
