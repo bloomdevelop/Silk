00ca9d95fd3a4fe4db206612de8c9d67de4373b9
StationaryStation <raulytstation@outlook.com>
2023-03-24 10:31:41 -0400

    chore(commands): Format

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,14 @@
 import { Client, Message } from "revolt.js";
 
 declare interface Command {
-    name: string,
-    description: string,
-    execute(message: Message, args?: string[], client: Client): Promise<any>,
-    use?: string,
-    cooldown?: number,
-    args?: boolean,
+    name: string;
+    description: string;
+    execute(
+        message: Message,
+        args?: string[],
+        client: Client
+    ): Promise<any>;
+    use?: string;
+    cooldown?: number;
+    args?: boolean;
 }
