93fa73e02b6daab38eac9ad1e56fb092eb264e19
StationaryStation <raulytstation@outlook.com>
2023-03-28 17:13:06 -0400

    Format whois command

--- src/commands/misc/whois.ts
+++ src/commands/misc/whois.ts
@@ -7,27 +7,39 @@ const whois: Command = {
     description: "Search for a user in cache",
     args: true,
     use: "<username>",
-    async execute (message: Message, args: string[]) {
-        let notFound: boolean = false;
-        const user = userCache.get(args[0]) || await message.client.users.fetch(args[0]).catch((e) => {
-            notFound = true;
-            return e;
-        });
+    async execute(message: Message, args: string[]) {
+        try {
+            const user =
+                userCache.get(args.join(" ")) ||
+                (await message.client.users.fetch(args[0]));
 
-        if (notFound) return message.channel?.sendMessage({
-            embeds: [{
-                title: "WhoIs",
-                description: `User not Found on Cache or Revolt`
-            }]
-        })
-        
-        return message.channel?.sendMessage({
-            embeds: [{
-                title: "WhoIs",
-                description: `Username: ${user?.username}\nProfile Picture: ${user?.generateAvatarURL()}\nUser ID: ${user?._id}\nIs Online: ${user?.online}\nIs on cache: ${userCache.get(user.username) ? true : false}`
-            }]
-        })
-    }
-}
+            return message.channel?.sendMessage({
+                embeds: [
+                    {
+                        title: "WhoIs",
+                        description: `Username: ${
+                            user?.username
+                        }\nProfile Picture: ${user?.generateAvatarURL()}\nUser ID: ${
+                            user?._id
+                        }\nIs Online: ${user?.online}\nIs on cache: ${
+                            userCache.get(user.username)
+                                ? true
+                                : false
+                        }`,
+                    },
+                ],
+            });
+        } catch {
+            return message.channel?.sendMessage({
+                embeds: [
+                    {
+                        title: "WhoIs",
+                        description: `User not Found on Cache or Revolt`,
+                    },
+                ],
+            });
+        }
+    },
+};
 
 export = whois;
