a5b217c8596cf1c80fc32a4bc3e552c22f6ee200
StationaryStation <raulytstation@outlook.com>
2023-03-30 10:34:13 -0400

    Make transtext usable

--- src/commands/fun/transtext.ts
+++ src/commands/fun/transtext.ts
@@ -1,17 +1,26 @@
 import { Message } from "revolt.js";
 import { Command } from "../../types";
+import { gradient, trans } from "../../util";
 
 const transtext: Command = {
     name: "transtext",
-    description: "Make the bot say something using the transexual flag's colourscheme (INCOMPLETE PLEASE FIX D:)",
+    description:
+        "Make the bot say something using the transexual flag's colourscheme (INCOMPLETE PLEASE FIX D:)",
     args: true,
     use: "<message>",
     async execute(message: Message, args: string[]) {
-        if (!args)
-            return message.reply("You didn't provide anything...");
+        try {
+            if (!args)
+                return message.reply(
+                    "You didn't provide anything..."
+                );
+            const sentence = args.join(" ");
+            const response = await trans(sentence);
 
-        const sentence = args.join(" ");
-        return await message.channel?.sendMessage(sentence);
+            return await message.channel?.sendMessage(response);
+        } catch (error) {
+            return await message.reply(`Failed to execute transtext, stacktrace:\n\`\`\`ts\n${error}\n\`\`\``)
+        }
     },
 };
 
---
+++ src/util/index.ts
@@ -0,0 +1,55 @@
+import chroma from "chroma-js";
+
+const gradient = (start: string, end: string, lenght: number) => {
+    const colours: string[] = [];
+
+    for (let i = 1; i <= lenght; i++) {
+        colours.push(chroma.mix(start, end, i / lenght).hex("rgb"));
+    }
+
+    return colours;
+};
+
+const defaultEscapes = new Map([
+    ["{", "\\{"],
+    ["}", "\\}"],
+    ["\\", "\\textbackslash"],
+    ["#", "\\#"],
+    ["$", "\\textdollar"],
+    ["%", "\\%"],
+    ["&", "\\&"],
+    ["^", "\\textasciicircum"],
+    ["_", "\\_"],
+    ["~", "\\textasciitilde"],
+]);
+
+const lescape = (c: string) => defaultEscapes.get(c) ?? c;
+
+const trans = async (s: string) => {
+    const colors = ["3ae", "e7b", "fff"];
+    const segments = Math.floor(s.length / 4);
+    const g = [
+        chroma(colors[0]).hex("rgb"),
+        ...gradient(colors[0], colors[1], segments),
+        ...gradient(colors[1], colors[2], segments),
+        ...gradient(colors[2], colors[1], segments),
+        ...gradient(
+            colors[1],
+            colors[0],
+            s.length - segments * 3 - 1
+        ),
+    ];
+
+    return s
+        .split("\n")
+        .map(
+            (s) =>
+                `\$\\textsf{${Array.from(s)
+                    .map(lescape)
+                    .map((c, i) => `\\color{${g[i]}}${c}`)
+                    .join("")}}\$`
+        )
+        .join("\n");
+};
+
+export { gradient, trans, lescape };
