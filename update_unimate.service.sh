e8a8dd17219ae3c16a1f25be574c91b728f29a98
Bloom-69 <129090449+Bloom-69@users.noreply.github.com>
2023-09-03 20:22:30 -0400

    upd: change the name of update script

--- update_unimate.service.sh
+++ update_unimate.service.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd ~/stationbot
+cd ~/unimate
 git remote update
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
-    	echo "Stationbot is up to date."
+    	echo "Unimate is up to date."
 elif [ $LOCAL = $BASE ]; then
-    	echo "Updating Stationbot..."
+    	echo "Unimate Stationbot..."
 	git pull
 	pnpm build
-	echo "StationBot Has been Updated"
+	echo "Unimate Has been Updated"
 fi
