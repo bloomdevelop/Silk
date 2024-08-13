c7efb2536fde7bd01a3de324b124bf0ea6755fe1
Bloom <bloomdevelop@proton.me>
2024-08-13 21:25:45 +0200

    feat: add WIP indicator and debug logging to kick command
    - Added a `wip` property to the `kick` command to indicate it is a work-in-progress
    - Added debug logging to the `kick` command to log the permissions of the user's roles

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,12 +1,20 @@
 import { ICommand } from "@/types";
+import { log } from "../../utilities/log";
 
 const kick: ICommand = {
-    name: 'kick',
-    description: 'Kicks to a user with reason provided',
-    usage: 'kick <userId> <reason>',
-    async execute (msg, args, client) {
-        
-    }
-}
+    name: "kick",
+    description: "Kicks to a user with reason provided",
+    usage: "kick <userId> <reason>",
+    wip: true,
+    async execute(msg, args, client) {
+        if (msg.member) {
+            for (const role of msg.member?.orderedRoles) {
+                if (role.permissions?.a) {
+                    log.debug(role.permissions.a << 6);
+                }
+            }
+        }
+    },
+};
 
 module.exports = kick;
