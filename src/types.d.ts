5ffaa30c83ef033e9965b709ec1de21e3533173e
Bloom <bloomdevelop@proton.me>
2024-08-13 21:24:31 +0200

    feat: add new configuration options
    - Added `wip` property to `ICommand` interface to indicate work-in-progress commands
    - Added `dangerous_command` and `blocked_users` properties to `IConfiguration` interface
    - Expanded the `experiments` property in `IConfiguration` to include `experimental_moderation`

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,6 +7,7 @@ declare interface ICommand {
     aliases?: string[];
     args?: boolean;
     cooldown?: number;
+    wip?: boolean;
     execute(
         message: Message,
         args?: string[],
@@ -17,7 +18,9 @@ declare interface ICommand {
 declare interface IConfiguration {
     disabled_commands: string[],
     prefix: string | "s?";
+    dangerous_command: boolean,
     experiments: {
         experimental_moderation: boolean
-    }
+    },
+    blocked_users: string[] // Useful when users abuses the votekick.
 }
