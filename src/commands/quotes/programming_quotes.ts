80ebe35c2d16f75b64d3f9ef93ced445a8baad3d
Bloom <bloomdevelop@proton.me>
2024-08-13 21:26:05 +0200

    feat: improve formatting of programming quotes command
    - Improved the formatting of the programming quotes command by:
      - Aligning the indentation and spacing of the code
      - Removing an unnecessary line break
      - Updating the fetch URL to use a single line

--- src/commands/quotes/programming_quotes.ts
+++ src/commands/quotes/programming_quotes.ts
@@ -1,25 +1,27 @@
 import { ICommand } from "@/types";
 
 const programming_quotes: ICommand = {
-  name: "programming_quotes",
-  description:
-    "Uses [Programming Quotes API](https://programming-quotesapi.vercel.app/) for get random quotes.",
-  usage: "programming_quote",
-  aliases: ["p_quote"],
-  async execute(msg) {
-    await fetch(`https://programming-quotesapi.vercel.app/api/random`)
-      .then((res) => res.json())
-      .then((data) => {
-        msg.reply({
-          embeds: [
-            {
-              title: "Programming Quotes",
-              description: `*"${data.quote}"*\n\- ${data.author}`,
-            },
-          ],
-        });
-      });
-  },
+    name: "programming_quotes",
+    description:
+        "Uses [Programming Quotes API](https://programming-quotesapi.vercel.app/) for get random quotes.",
+    usage: "programming_quote",
+    aliases: ["p_quote"],
+    async execute(msg) {
+        await fetch(
+            `https://programming-quotesapi.vercel.app/api/random`,
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                msg.reply({
+                    embeds: [
+                        {
+                            title: "Programming Quotes",
+                            description: `*"${data.quote}"*\n\- ${data.author}`,
+                        },
+                    ],
+                });
+            });
+    },
 };
 
 module.exports = programming_quotes;
